5bb126fbd8266d06da685444e2cba7ef2742b338
karim sayed <76128546+karimsayed3@users.noreply.github.com>
2023-03-09 03:27:57 +0200

    final

--- src/routes/quiz.ts
+++ src/routes/quiz.ts
@@ -22,23 +22,6 @@ router.post(
   "/",
   isAuthenticated,
   [
-    body("name")
-      .trim()
-      .not()
-      .isEmpty()
-      .isLength({ min: 10 })
-      .withMessage("Please enter a valid name, minimum 10 character long")
-      .custom((name) => {
-        return isValidQuizName(name)
-          .then((status: Boolean) => {
-            if (!status) {
-              return Promise.reject("Plaase enter an unique quiz name.");
-            }
-          })
-          .catch((err) => {
-            return Promise.reject(err);
-          });
-      }),
     body("questions_list").custom((questions_list, { req }) => {
       return isValidQuiz(questions_list, req.body["answers"])
         .then((status: Boolean) => {
