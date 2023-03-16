8b685ed5bc107fcc3ea806d5cabec99ff11fb53c
karim sayed <76128546+karimsayed3@users.noreply.github.com>
2023-03-16 02:17:11 +0200

    karim

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -7,5 +7,5 @@ const router = express.Router();
 
 //GET /report/:reportId
 router.get("/:reportId?", isAuthenticated, getReport);
-
+router.get("/", isAuthenticated, getReport);
 export default router;
